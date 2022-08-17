2380696ac0da47373d26daee6941dfe053eda662
Andrew Nourry <andrewnourry@gmail.com>
2022-08-17 04:36:11 +0200

    add Review and Ticket classes

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.CharField(max_length=128, verbose_name="titre")
+    comment = models.CharField(max_length=6000, verbose_name="commentaire")
+    original_poster = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128, verbose_name="titre")
+    description = models.CharField(max_length=6000, verbose_name="description")
+    requester = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='demandeur')
+    image = models.ImageField(verbose_name="image")
+    review = models.ForeignKey(Review, null=True, on_delete=models.SET_NULL, blank=True)
