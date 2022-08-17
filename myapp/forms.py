b5a27387b7e637b20f846f465500d2313ad35177
Andrew Nourry <andrewnourry@gmail.com>
2022-08-17 04:37:24 +0200

    add ReviewForm and TicketForm classes

---
+++ myapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from . import models
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = models.Review
+        fields = ["title", "comment"]
+
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = models.Ticket
+        fields = ["title", "description", "image"]
