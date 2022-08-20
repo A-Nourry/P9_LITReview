9abacb49c37d515de5ae8ba9f89f57e2c2db268f
Andrew Nourry <andrewnourry@gmail.com>
2022-08-20 02:12:24 +0200

    Review:
    rating added,
    ticket foreignKey added
    
    Ticket:
    
    review foreignKey removed

--- myapp/models.py
+++ myapp/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from django.conf import settings
-
-
-class Review(models.Model):
-    title = models.CharField(max_length=128, verbose_name="titre")
-    comment = models.CharField(max_length=6000, verbose_name="commentaire")
-    original_poster = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Ticket(models.Model):
     title = models.CharField(max_length=128, verbose_name="titre")
     description = models.CharField(max_length=6000, verbose_name="description")
-    requester = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='demandeur')
+    requester = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="demandeur"
+    )
     image = models.ImageField(verbose_name="image")
-    review = models.ForeignKey(Review, null=True, on_delete=models.SET_NULL, blank=True)
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=128, verbose_name="titre")
+    comment = models.CharField(max_length=6000, verbose_name="commentaire")
+    rating = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    original_poster = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    ticket = models.ForeignKey(Ticket, null=True, on_delete=models.SET_NULL, blank=True)
