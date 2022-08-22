ecde155bd161104b83f510a3d6f2c3633520f3b3
Andrew Nourry <andrewnourry@gmail.com>
2022-08-22 10:25:04 +0200

    ajouts classes:
    FollowUsersForm
    DeletePostForm

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,15 +1,30 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 from . import models
 
 
 class ReviewForm(forms.ModelForm):
+    edit_review = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
     class Meta:
         model = models.Review
         fields = ["title", "rating", "comment"]
 
 
 class TicketForm(forms.ModelForm):
+    edit_ticket = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
     class Meta:
         model = models.Ticket
         fields = ["title", "description", "image"]
+
+
+class FollowUsersForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ["follows"]
+
+
+class DeletePostForm(forms.Form):
+    delete_post = forms.BooleanField(widget=forms.HiddenInput, initial=True)
