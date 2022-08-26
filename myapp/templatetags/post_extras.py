1f0407e327420f96aa94b53736bd7e406e4a6942
Andrew Nourry <andrewnourry@gmail.com>
2022-08-26 12:13:57 +0200

    initial commit

---
+++ myapp/templatetags/post_extras.py
@@ -0,0 +1,24 @@
+from django import template
+
+MINUTE = 60
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
+
+register = template.Library()
+
+
+@register.filter
+def model_type(value):
+    return type(value).__name__
+
+
+@register.filter
+def get_posted_at_display(posted_at):
+    return f'{posted_at.strftime("%Hh%M, %d %b %y")}'
+
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if user == context["user"]:
+        return "vous"
+    return user.username
