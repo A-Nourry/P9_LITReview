59692cbe05b6b2bf7623c9a533d891978d19bad0
Andrew Nourry <andrewnourry@gmail.com>
2022-08-31 05:25:14 +0200

    LoginForm added SignupForm updated

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,10 +1,54 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Nom d'utilisateur",
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Mot de passe",
+        })
+    )
 
 
 class SignupForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Nom d'utilisateur",
+            }
+        )
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Mot de passe",
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirmer mot de passe",
+            }
+        )
+    )
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = [
-            "username",
-        ]
+        fields = ["username", "password1", "password2"]
