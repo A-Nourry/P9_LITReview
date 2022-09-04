4e2cfd7f46729465f6b46fadecfc3c1149e45d7a
Andrew Nourry <andrewnourry@gmail.com>
2022-09-04 21:43:46 +0200

    update subscriptions view

--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,29 @@
 from itertools import chain
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.db.models import CharField, Value
 from django.db.models import Q
 
 from . import forms, models
 
+User = get_user_model()
+
 
 @login_required
 def feed(request):
-    reviews = models.Review.objects.filter(
-        Q(original_poster__in=request.user.follows.all())
-        | Q(original_poster__exact=request.user.id)
-    )
-    reviews = reviews.annotate(content_type=Value("REVIEW", CharField()))
-
     tickets = models.Ticket.objects.filter(
         Q(requester__in=request.user.follows.all())
         | Q(requester__exact=request.user.id)
     )
     tickets = tickets.annotate(content_type=Value("TICKET", CharField()))
+
+    reviews = models.Review.objects.filter(
+        Q(original_poster__in=request.user.follows.all())
+        | Q(original_poster__exact=request.user.id)
+        | Q(ticket__requester=request.user)
+    )
+    reviews = reviews.annotate(content_type=Value("REVIEW", CharField()))
 
     posts = sorted(
         chain(reviews, tickets), key=lambda post: post.date_created, reverse=True
@@ -51,17 +55,33 @@ def my_posts(request):
 
 @login_required
 def subscriptions(request):
-    users_followed = request.user.follows
-    form = forms.FollowUsersForm(instance=request.user)
+    followed = request.user.follows.all()
+    followers = User.objects.filter(follows=request.user.id)
+    follow_form = forms.FollowUsersForm()
+    message = ""
+
     if request.method == "POST":
-        form = forms.FollowUsersForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect("subscriptions")
+        if "unfollow_user" in request.POST:
+            delete_form = forms.UnfollowsUserForm(request.POST)
+            if delete_form.is_valid():
+                request.user.follows.remove()
+                return redirect("subscriptions")
+
+        if follow_form.is_valid():
+            follow_form.save(commit=False)
+            following_user = User.objects.get(username="david")
+            if following_user in User.objects.all():
+                request.user.follows.add(following_user)
+                request.user.save()
+                return redirect("subscriptions")
+
+        message = "Nom d'utilisateur invalide."
 
     context = {
-        "form": form,
-        "users": users_followed,
+        "form": follow_form,
+        "followed": followed,
+        "followers": followers,
+        "message": message,
     }
 
     return render(request, "myapp/subscriptions.html", context=context)
@@ -114,6 +134,7 @@ def change_review(request, review_id):
     review = get_object_or_404(models.Review, ticket_id=review_id)
     ticket = get_object_or_404(models.Ticket, id=review_id)
     edit_form = forms.EditReviewForm(instance=review)
+    user = review.original_poster
     delete_form = forms.DeletePostForm()
     if request.method == "POST":
         if "edit_review" in request.POST:
@@ -131,17 +152,23 @@ def change_review(request, review_id):
         "delete_form": delete_form,
         "ticket": ticket,
     }
-    return render(request, "myapp/change_review.html", context=context)
+    if request.user == user:
+        return render(request, "myapp/change_review.html", context=context)
+    else:
+        return redirect("feed")
 
 
 @login_required
 def change_ticket(request, ticket_id):
     ticket = get_object_or_404(models.Ticket, id=ticket_id)
+    user = ticket.requester
     edit_form = forms.EditTicketForm(instance=ticket)
     delete_form = forms.DeletePostForm()
     if request.method == "POST":
         if "edit_ticket" in request.POST:
-            edit_form = forms.EditTicketForm(request.POST, request.FILES, instance=ticket)
+            edit_form = forms.EditTicketForm(
+                request.POST, request.FILES, instance=ticket
+            )
             if edit_form.is_valid():
                 edit_form.save()
                 return redirect("feed")
@@ -155,7 +182,10 @@ def change_ticket(request, ticket_id):
         "delete_form": delete_form,
         "ticket": ticket,
     }
-    return render(request, "myapp/change_ticket.html", context=context)
+    if request.user == user:
+        return render(request, "myapp/change_ticket.html", context=context)
+    else:
+        return redirect("feed")
 
 
 @login_required
